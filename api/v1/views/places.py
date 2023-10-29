08db979679b45ac594a0684f6f1ec20f1647fdb0
kumdan job <redeks123456@gmail.com>
2023-10-29 22:27:57 +0100

    task 10 - 11

--- api/v1/views/places.py
+++ api/v1/views/places.py
@@ -10,7 +10,9 @@ from models.place import Place
 from models.user import User
 
 
-@app_views.route("/cities/<city_id>/places", methods=["GET"], strict_slashes=False)
+@app_views.route(
+    "/cities/<string:city_id>/places", methods=["GET"], strict_slashes=False
+)
 def get_place_cities(city_id):
     """return a list of places in the city object"""
     city = storage.get(City, city_id)
@@ -23,7 +25,7 @@ def get_place_cities(city_id):
         return jsonify([cit.to_dict() for cit in place])
 
 
-@app_views.route("/places/<place_id>", methods=["GET"], strict_slashes=False)
+@app_views.route("/places/<string:place_id>", methods=["GET"], strict_slashes=False)
 @swag_from("documentation/place/get_id.yml", methods=["GET"])
 def get_place_id(place_id):
     """Retrieves a specific place by id"""
@@ -45,7 +47,9 @@ def delete_place(place_id):
     return make_response(jsonify({}), 200)
 
 
-@app_views.route("/cities/<city_id>/places", methods=["POST"], strict_slashes=False)
+@app_views.route(
+    "/cities/<string:city_id>/places", methods=["POST"], strict_slashes=False
+)
 @swag_from("documentation/city/post_city.yml", methods=["POST"])
 def post_place(city_id):
     """
@@ -72,7 +76,7 @@ def post_place(city_id):
     return make_response(jsonify(instance.to_dict()), 201)
 
 
-@app_views.route("/places/<place_id>", methods=["PUT"], strict_slashes=False)
+@app_views.route("/places/<string:place_id>", methods=["PUT"], strict_slashes=False)
 @swag_from("documentation/place/put_place.yml", methods=["PUT"])
 def put_place(place_id):
     """put place change the values of the place"""
