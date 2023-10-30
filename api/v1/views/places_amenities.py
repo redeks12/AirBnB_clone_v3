df6a35c4c09a839b9356a0ed91d9b472ad83dd95
kemelite1 <osikemelite42@gmail.com>
2023-10-30 10:24:50 +0100

    number 13. a new view for the link between Place objects and Amenity objects that handles all default RESTFul API actions

---
+++ api/v1/views/places_amenities.py
@@ -0,0 +1,53 @@
+#!/usr/bin/python3
+"""a new view for the link between Place objects and Amenity objects that handles all default RESTFul API actions"""
+from flasgger.utils import swag_from
+from flask import abort, jsonify, make_response, request
+
+from api.v1.views import app_views
+from models import storage
+from models.place import Place
+from models.amenity import Amenity
+
+@app_views.route('/places/<string:place_id>/amenities', methods=['GET'], strict_slashes=False)
+@swag_from('documentation/place_amenity/get_id.yml', methods=['GET'])
+def get_place_amenities(place_id):
+    """ Retrieves a list of all amenities from a place """
+    place = storage.get(Place, place_id)
+    if place is None:
+        abort(404)
+    amenities = [obj.to_dict() for obj in place.amenities]
+    return jsonify(amenities)
+
+
+@app_views.route('/places/<string:place_id>/amenities/<string:amenity_id>', methods=['DELETE'], strict_slashes=False)
+@swag_from('documentation/place_amenity/delete.yml', methods=['DELETE'])
+def delete_place_amenity(place_id, amenity_id):
+    """ Deletes an  amenity from place """
+    place = storage.get(Place, place_id)
+    if place is None:
+        abort(404)
+    amenity = storage.get(Amenity, amenity_id)
+    if amenity is None:
+        abort(404)
+    if amenity not in place.amenities:
+        abort(404)
+    place.amenities.remove(amenity)
+    storage.save()
+    return jsonify({})
+
+
+@app_views.route('/places/<string:place_id>/amenities/<string:amenity_id>', methods=['POST'], strict_slashes=False)
+@swag_from('documentation/place_amenity/post.yml', methods=['POST'])
+def post_place_amenity2(place_id, amenity_id):
+    """ post a place amenity by id """
+    place = storage.get(Place, place_id)
+    if place is None:
+        abort(404)
+    amenity = storage.get(Amenity, amenity_id)
+    if amenity is None:
+        abort(404)
+    if amenity in place.amenities:
+        return (jsonify(amenity.to_dict()), 200)
+    place.amenities.append(obj)
+    storage.save()
+    return (jsonify(amenity.to_dict(), 201))
