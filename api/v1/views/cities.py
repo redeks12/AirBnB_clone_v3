6a2ae0fc4aec43edfe61e7c86179fe518e9fb936
kumdan job <redeks123456@gmail.com>
2023-10-30 11:51:59 +0100

    task 10 - 11

--- api/v1/views/cities.py
+++ api/v1/views/cities.py
@@ -73,7 +73,7 @@ def put_city(city_id):
     if not city:
         abort(404)
     if not request.get_json():
-        return abort(400, description="Not a JSON")
+        abort(400, description="Not a JSON")
 
     ignore = ["id", "state_id", "created_at", "updated_at"]
     for key, val in dict(request.get_json()).items():
